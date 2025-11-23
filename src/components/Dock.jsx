8241fa72d54198f2e5337d2935f68334e5b5a954
dulee19 <adusan53@gmail.com>
2025-11-23 12:35:33 +0100

    zustand, immer, window store

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,11 @@ import { dockApps } from "../constants";
 import { Tooltip } from "react-tooltip";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import useWindowStore from "../store/window";
 
 const Dock = () => {
+  const { openWindow, closeWindow, windows } = useWindowStore();
+
   const dockRef = useRef(null);
 
   useGSAP(() => {
@@ -49,13 +52,26 @@ const Dock = () => {
     dock.addEventListener("mouseleave", resetIcons);
 
     return () => {
-        dock.removeEventListener("mousemove", handleMouseMove);
-        dock.removeEventListener("mouseleave", resetIcons);
+      dock.removeEventListener("mousemove", handleMouseMove);
+      dock.removeEventListener("mouseleave", resetIcons);
     }
   }, [])
 
   const toggleApp = (app) => {
-    
+    if (!app.canOpen) return;
+
+    const window = windows[app.id];
+
+    if (!window) {
+      console.error(`Window not found for app: ${app.id}`);
+      return;
+    }
+
+    if (window.isOpen) {
+      closeWindow(app.id);
+    } else {
+      openWindow(app.id);
+    }
   }
 
   return (
